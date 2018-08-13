e2ae44fe501b6798b6eb21d47ba1162c104d16a4
oyuk <okysoft68@gmail.com>
2018-08-13 10:50:44 +0900

    queryのdefaultの書き方を変えた

--- app/src/main/java/com/okysoft/annictim/API/UserServiceImpl.kt
+++ app/src/main/java/com/okysoft/annictim/API/UserServiceImpl.kt
@@ -3,7 +3,7 @@ package com.okysoft.annictim.API
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.rx2.Rx2Apollo
 import com.okysoft.annictim.API.Model.Response.User
-import com.okysoft.annictim.ApolloApi.MeQuery
+import com.okysoft.annictim.ApolloApi.Me
 import com.okysoft.annictim.Result
 import io.reactivex.Single
 import javax.inject.Inject
@@ -11,7 +11,7 @@ import javax.inject.Inject
 class UserServiceImpl @Inject constructor(private val client: ApolloClient): AnnictService.User {
 
     override fun getMe(): Single<Result<User>> {
-        return Rx2Apollo.from(client.query(MeQuery())).map {
+        return Rx2Apollo.from(client.query(Me())).map {
             it.data()?.viewer()?.let {
                 return@map Result.success(User(
                         name = it.name(),
