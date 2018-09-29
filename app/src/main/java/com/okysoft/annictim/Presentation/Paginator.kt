f42db8fc71967d1d7b587bce1a94d6dff48266b1
oyuk <okysoft68@gmail.com>
2018-09-29 17:10:17 +0900

    recyclerviewのpagination

---
+++ app/src/main/java/com/okysoft/annictim/Presentation/Paginator.kt
@@ -0,0 +1,11 @@
+package com.okysoft.annictim.Presentation
+
+import android.support.v7.widget.RecyclerView
+
+class Paginator(private val recyclerView: RecyclerView) {
+
+    fun start() {
+
+    }
+
+}
