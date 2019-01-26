be58715aa026ce345cec9eef5ebec0ac6bdf78e0
oyuk <okysoft68@gmail.com>
2019-01-27 00:34:40 +0900

    responseのモデルを追加

---
+++ app/src/main/java/com/okysoft/annictim/api/model/response/People.kt
@@ -0,0 +1,53 @@
+package com.okysoft.annictim.api.model.response
+import com.google.gson.annotations.SerializedName
+
+
+data class People(
+    @SerializedName("birthday")
+    val birthday: String,
+    @SerializedName("blood_type")
+    val bloodType: String,
+    @SerializedName("casts_count")
+    val castsCount: Int,
+    @SerializedName("favorite_people_count")
+    val favoritePeopleCount: Int,
+    @SerializedName("gender_text")
+    val genderText: String,
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("name_en")
+    val nameEn: String,
+    @SerializedName("name_kana")
+    val nameKana: String,
+    @SerializedName("nickname")
+    val nickname: String,
+    @SerializedName("nickname_en")
+    val nicknameEn: String,
+    @SerializedName("prefecture")
+    val prefecture: Prefecture,
+    @SerializedName("staffs_count")
+    val staffsCount: Int,
+    @SerializedName("twitter_username")
+    val twitterUsername: String,
+    @SerializedName("twitter_username_en")
+    val twitterUsernameEn: String,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("url_en")
+    val urlEn: String,
+    @SerializedName("wikipedia_url")
+    val wikipediaUrl: String,
+    @SerializedName("wikipedia_url_en")
+    val wikipediaUrlEn: String
+) {
+    data class Prefecture(
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("name")
+        val name: String
+    )
+}
---
+++ app/src/main/java/com/okysoft/annictim/presentation/activity/CastsActivity.kt
@@ -0,0 +1,12 @@
+package com.okysoft.annictim.presentation.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CastsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_casts)
+    }
+}
