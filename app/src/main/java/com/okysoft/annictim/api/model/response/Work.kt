7f75b25bdd9766b9170ebab2b4959a0de7db6c55
oyuk <okysoft68@gmail.com>
2019-01-27 00:09:11 +0900

    review_countをworkitemで表示するようにした

--- app/src/main/java/com/okysoft/annictim/api/model/response/Work.kt
+++ app/src/main/java/com/okysoft/annictim/api/model/response/Work.kt
@@ -6,24 +6,25 @@ import paperparcel.PaperParcelable
 
 @PaperParcel
 data class Work(
-        @SerializedName("id") val id: Int,
-        @SerializedName("title") val title: String,
-        @SerializedName("title_kana") val titleKana: String?,
-        @SerializedName("media") val media: String?,
-        @SerializedName("media_text") val mediaText: String?,
-        @SerializedName("season_name") val seasonName: String?,
-        @SerializedName("season_name_text") val seasonNameText: String?,
-        @SerializedName("released_on") val releasedOn: String?,
-        @SerializedName("released_on_about") val releasedOnAbout: String?,
-        @SerializedName("official_site_url") val officialSiteUrl: String?,
-        @SerializedName("wikipedia_url") val wikipediaUrl: String?,
-        @SerializedName("twitter_username") val twitterUsername: String?,
-        @SerializedName("twitter_hashtag") val twitterHashtag: String?,
-        @SerializedName("mal_anime_id") val malAnimeId: String?,
-        @SerializedName("images") val images: Images,
-        @SerializedName("episodes_count") val episodesCount: Int?,
-        @SerializedName("watchers_count") val watchersCount: Int?
-): PaperParcelable {
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("title_kana") val titleKana: String?,
+    @SerializedName("media") val media: String?,
+    @SerializedName("media_text") val mediaText: String?,
+    @SerializedName("season_name") val seasonName: String?,
+    @SerializedName("season_name_text") val seasonNameText: String?,
+    @SerializedName("released_on") val releasedOn: String?,
+    @SerializedName("released_on_about") val releasedOnAbout: String?,
+    @SerializedName("official_site_url") val officialSiteUrl: String?,
+    @SerializedName("wikipedia_url") val wikipediaUrl: String?,
+    @SerializedName("twitter_username") val twitterUsername: String?,
+    @SerializedName("twitter_hashtag") val twitterHashtag: String?,
+    @SerializedName("mal_anime_id") val malAnimeId: String?,
+    @SerializedName("images") val images: Images,
+    @SerializedName("episodes_count") val episodesCount: Int?,
+    @SerializedName("watchers_count") val watchersCount: Int?,
+    @SerializedName("reviews_count") val reviewsCount: Int?
+    ): PaperParcelable {
 
     companion object {
         @JvmField val CREATOR = PaperParcelWork.CREATOR
