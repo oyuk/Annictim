86b78ff5ff7e26bdc66efcca0e723fc3aecc1502
oyuk <okysoft68@gmail.com>
2019-01-27 19:02:29 +0900

    CastViewModel

---
+++ app/src/main/java/com/okysoft/annictim/presentation/viewModel/CastsViewModel.kt
@@ -0,0 +1,39 @@
+package com.okysoft.annictim.presentation.viewModel
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.okysoft.annictim.api.model.response.Cast
+import com.okysoft.annictim.api.repository.CastRepository
+import com.okysoft.annictim.presentation.CastPaginator
+import com.okysoft.annictim.presentation.CastRequestParams
+import com.okysoft.annictim.toLiveData
+import io.reactivex.processors.PublishProcessor
+import javax.inject.Inject
+
+class CastsViewModel constructor(
+    repository: CastRepository,
+    castRequestParams: CastRequestParams
+) : ViewModel() {
+
+    class Factory @Inject constructor(
+        private val repository: CastRepository,
+        private val castRequestParams: CastRequestParams
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return CastsViewModel(repository, castRequestParams) as T
+        }
+    }
+
+    val loadMore = PublishProcessor.create<Unit>()
+    private val paginator: CastPaginator = CastPaginator(loadMore) { page ->
+        repository.get(castRequestParams.copy(page = page).toParams())
+    }
+    val casts: LiveData<List<Cast>> = paginator.items.toLiveData()
+
+    fun refresh() {
+        paginator.refresh()
+    }
+
+}
