defe7066f7c103350b7e2fe920dbae8b2046e77e
oyuk <okysoft68@gmail.com>
2019-03-10 00:28:50 +0900

    meのリクエストが送られていないので修正

--- app/src/main/java/com/okysoft/annictim/presentation/works/WorksViewModel.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/works/WorksViewModel.kt
@@ -3,11 +3,11 @@ package com.okysoft.annictim.presentation.works
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.okysoft.annictim.usecase.WorkUseCase
 import com.okysoft.annictim.domain.Work
 import com.okysoft.annictim.extension.toLiveData
 import com.okysoft.annictim.infra.api.model.request.WorkRequestParams
 import com.okysoft.annictim.presentation.WorkPaginator
+import com.okysoft.annictim.usecase.WorkUseCase
 import io.reactivex.Single
 import io.reactivex.processors.PublishProcessor
 import kotlinx.coroutines.Job
@@ -40,7 +40,7 @@ class WorksViewModel constructor(
     init {
         val context = coroutineContext + job
         val requestCreator: ((Int) -> Single<List<Work>>) = { page ->
-            useCase.get(workRequestParams.copy(page = page))
+            useCase.request(workRequestParams.copy(page = page))
         }
         paginator = WorkPaginator(loadMore, refresh, requestCreator)
         works = paginator.items.toLiveData()
