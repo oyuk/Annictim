5c2fba5b4688fcb8a688188f21e93fb6ae1697a4
oyuk <okysoft68@gmail.com>
2019-03-10 01:19:53 +0900

    user使用箇所を修正

--- app/src/main/java/com/okysoft/annictim/presentation/user/UserViewModel.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/user/UserViewModel.kt
@@ -1,46 +1,44 @@
 package com.okysoft.annictim.presentation.user
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import android.util.Log
-import com.okysoft.annictim.infra.api.repository.AuthRepository
-import com.okysoft.annictim.infra.api.model.response.UserResponse
-import com.okysoft.annictim.infra.api.repository.UserRepository
+import com.okysoft.annictim.domain.User
+import com.okysoft.annictim.usecase.UserUseCase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.await
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class UserViewModel constructor(
-    private val userRepository: UserRepository,
-    private val authRepository: AuthRepository,
+    private val userUseCase: UserUseCase,
     private val coroutineContext: CoroutineContext
 ): ViewModel() {
 
     class Factory @Inject constructor(
-        private val userRepository: UserRepository,
-        private val authRepository: AuthRepository,
+        private val userUseCase: UserUseCase,
         private val coroutineContext: CoroutineContext
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return UserViewModel(userRepository, authRepository, coroutineContext) as T
+            return UserViewModel(userUseCase, coroutineContext) as T
         }
     }
 
     private val job = Job()
     var userId: Int = 0
-    private val _user = MutableLiveData<UserResponse>()
-    val user: LiveData<UserResponse> = _user
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User> = _user
 
     fun fetch() {
          GlobalScope.launch(coroutineContext + job) {
              try {
-                 val userResponse = userRepository.get(userId).await()
-                 val user = userResponse.users.first()
+                 val userResponse = userUseCase.get(userId).await()
+                 val user = userResponse.first()
                  _user.postValue(user)
              } catch (throwable: Throwable) {
                  Log.d("", throwable.toString())
