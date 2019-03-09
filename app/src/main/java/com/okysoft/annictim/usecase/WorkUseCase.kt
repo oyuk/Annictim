efb27ccb6dc200e1ce78f358f04a4156b6c08dcc
oyuk <okysoft68@gmail.com>
2019-03-10 00:28:50 +0900

    meのリクエストが送られていないので修正

--- app/src/main/java/com/okysoft/annictim/usecase/WorkUseCase.kt
+++ app/src/main/java/com/okysoft/annictim/usecase/WorkUseCase.kt
@@ -1,9 +1,9 @@
 package com.okysoft.annictim.usecase
 
-import com.okysoft.annictim.translator.WorkTranslator
 import com.okysoft.annictim.domain.Work
 import com.okysoft.annictim.infra.api.model.request.WorkRequestParams
 import com.okysoft.annictim.infra.api.repository.WorkRepository
+import com.okysoft.annictim.translator.WorkTranslator
 import io.reactivex.Single
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.rx2.rxSingle
@@ -11,10 +11,8 @@ import kotlinx.coroutines.rx2.rxSingle
 interface WorkUseCase {
 
     fun get(requestParams: WorkRequestParams): Single<List<Work>>
-
-
     fun me(requestParams: WorkRequestParams): Single<List<Work>>
-
+    fun request(requestParams: WorkRequestParams): Single<List<Work>>
 }
 
 class WorkUseCaseImpl (
@@ -35,6 +33,13 @@ class WorkUseCaseImpl (
             val response = repository.me(requestParams).await()
             val models = response.works.map { translator.translate(it) }
             return@rxSingle models
+        }
+    }
+
+    override fun request(requestParams: WorkRequestParams): Single<List<Work>> {
+        return when (requestParams.type) {
+            WorkRequestParams.Type.Me -> me(requestParams)
+            WorkRequestParams.Type.Works -> get((requestParams))
         }
     }
 
