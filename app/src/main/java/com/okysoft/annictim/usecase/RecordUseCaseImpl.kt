6f274d05c610262ed8dbdc02748c743b66c8fa26
oyuk <okysoft68@gmail.com>
2019-03-10 01:27:08 +0900

    recordのtranslatorとusecase

---
+++ app/src/main/java/com/okysoft/annictim/usecase/RecordUseCaseImpl.kt
@@ -0,0 +1,23 @@
+package com.okysoft.annictim.usecase
+
+import com.okysoft.annictim.domain.Record
+import com.okysoft.annictim.infra.api.repository.RecordRepository
+import com.okysoft.annictim.translator.RecordTranslator
+import io.reactivex.Single
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.rx2.rxSingle
+
+class RecordUseCaseImpl (
+    private val repository: RecordRepository,
+    private val translator: RecordTranslator
+): RecordUseCase {
+
+    override fun get(workId: Int): Single<List<Record>> {
+        return GlobalScope.rxSingle {
+            val response = repository.get(workId).await()
+            val models = response.records.map { translator.translate(it) }
+            return@rxSingle models
+        }
+    }
+
+}
