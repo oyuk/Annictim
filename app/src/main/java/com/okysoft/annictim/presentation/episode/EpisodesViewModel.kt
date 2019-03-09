ce0a2b5d2d42250814c2760e8d1bb58971d58e8d
oyuk <okysoft68@gmail.com>
2019-03-10 01:07:24 +0900

    episode使用箇所を修正

--- app/src/main/java/com/okysoft/annictim/presentation/episode/EpisodesViewModel.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/episode/EpisodesViewModel.kt
@@ -5,40 +5,41 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.okysoft.annictim.infra.api.model.response.EpisodeResponse
-import com.okysoft.annictim.infra.api.repository.EpisodeRepository
+import com.okysoft.annictim.domain.Episode
+import com.okysoft.annictim.usecase.EpisodeUseCase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.await
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class EpisodesViewModel @Inject constructor (
         private val workId: Int,
-        private val episodeRepository: EpisodeRepository,
+        private val useCase: EpisodeUseCase,
         private val coroutineContext: CoroutineContext
 ): ViewModel() {
 
     class Factory @Inject constructor (
             private val workId: Int,
-            private val repository: EpisodeRepository,
+            private val useCase: EpisodeUseCase,
             private val coroutineContext: CoroutineContext
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return EpisodesViewModel(workId, repository, coroutineContext) as T
+            return EpisodesViewModel(workId, useCase, coroutineContext) as T
         }
     }
 
     private val job = Job()
-    private val _episodes = MutableLiveData<List<EpisodeResponse>>()
-    val episodes: LiveData<List<EpisodeResponse>> = _episodes
+    private val _episodes = MutableLiveData<List<Episode>>()
+    val episodes: LiveData<List<Episode>> = _episodes
 
     fun fetch() {
         GlobalScope.launch(coroutineContext + job) {
             try  {
-                val response = episodeRepository.get(workId).await()
-                _episodes.postValue(response.episodes)
+                val response = useCase.get(workId).await()
+                _episodes.postValue(response)
             } catch (throwable: Throwable) {
                 Log.d("", throwable.toString())
 
