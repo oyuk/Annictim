9acafd89fb229c11b12c1eadfbffc9d550a047a3
oyuk <okysoft68@gmail.com>
2019-03-10 00:44:59 +0900

    reviewを使う箇所を修正

--- app/src/main/java/com/okysoft/annictim/presentation/review/ReviewsViewModel.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/review/ReviewsViewModel.kt
@@ -5,28 +5,29 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.okysoft.annictim.infra.api.model.response.Review
-import com.okysoft.annictim.infra.api.repository.ReviewRepository
+import com.okysoft.annictim.domain.Review
+import com.okysoft.annictim.usecase.ReviewUseCase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.await
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class ReviewsViewModel @Inject constructor (
         private val workId: Int,
-        private val reviewRepository: ReviewRepository,
+        private val useCase: ReviewUseCase,
         private val coroutineContext: CoroutineContext
 ): ViewModel() {
 
     class Factory @Inject constructor (
             private val workId: Int,
-            private val repository: ReviewRepository,
+            private val useCase: ReviewUseCase,
             private val coroutineContext: CoroutineContext
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return ReviewsViewModel(workId, repository, coroutineContext) as T
+            return ReviewsViewModel(workId, useCase, coroutineContext) as T
         }
     }
 
@@ -37,8 +38,8 @@ class ReviewsViewModel @Inject constructor (
     init {
         GlobalScope.launch(coroutineContext + job) {
             try {
-                val response = reviewRepository.get(workId).await()
-                _reviews.postValue(response.reviews)
+                val response = useCase.get(workId).await()
+                _reviews.postValue(response)
             } catch (throwable: Throwable) {
                 Log.d("", throwable.toString())
             }
