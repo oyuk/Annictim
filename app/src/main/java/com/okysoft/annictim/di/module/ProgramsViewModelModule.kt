4df2838b272910b5f9bca726f0071518d8a8d558
oyuk <okysoft68@gmail.com>
2020-08-27 12:33:42 +0900

    未使用なので削除

--- app/src/main/java/com/okysoft/annictim/di/module/ProgramsViewModelModule.kt
+++
@@ -1,6 +0,0 @@
-package com.okysoft.annictim.di.module
-
-//@InstallIn(FragmentComponent::class)
-//@AssistedModule
-//@Module(includes = [AssistedInject_ProgramsViewModelModule::class])
-//interface ProgramsViewModelModule
