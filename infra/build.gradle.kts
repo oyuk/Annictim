15758a1381f81035049c0a2883ec9bfbacfc012b
oyuk <okysoft68@gmail.com>
2021-04-11 01:19:11 +0900

    ktsにした

---
+++ infra/build.gradle.kts
@@ -0,0 +1,89 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("com.apollographql.apollo")
+}
+
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(22)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        val propertiesFile = file(project.rootProject.file("local.properties"))
+        if (propertiesFile.exists()) {
+            val properties = Properties()
+            properties.load(propertiesFile.inputStream())
+            buildConfigField("String", "annictClientId", properties.getProperty("CLIENT_ID"))
+            buildConfigField("String", "annictClientKey", properties.getProperty("CLIENT_KEY"))
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    apollo {
+        // instruct the compiler to generate Kotlin models
+        generateKotlinModels.set(true)
+
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Dependencies.Version.kotlin}")
+
+    testImplementation("junit:junit:4.13.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.8.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.8.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.4.1")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    val daggerVersion = "2.31.2"
+    compileOnly("javax.annotation:jsr250-api:1.0")
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    implementation("com.google.dagger:dagger-android-support:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+
+    implementation("com.google.dagger:hilt-android:2.31.2-alpha")
+    kapt("com.google.dagger:hilt-android-compiler:2.31.2-alpha")
+
+
+    implementation("com.apollographql.apollo:apollo-runtime:2.2.0")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.2.0")
+
+    implementation(project(":data"))
+    implementation(project(":common"))
+}
