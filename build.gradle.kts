51a0f64ad3b752ef08a28739f370c927d15c5b83
oyuk <okysoft68@gmail.com>
2021-04-11 01:19:05 +0900

    ktsにした

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    apply(from = "${rootDir.absolutePath}/deps.gradle")
+
+    val kotlinVersion by extra { "1.4.32" }
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.3")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.31.2-alpha")
+        classpath("com.apollographql.apollo:apollo-gradle-plugin:2.2.0")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
