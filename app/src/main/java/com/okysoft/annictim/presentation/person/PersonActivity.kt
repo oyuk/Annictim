7a036ff1ea8fea08c7fe6a6ae71643796528b479
oyuk <okysoft68@gmail.com>
2021-10-01 16:58:29 +0900

    personのfragment, activity

--- app/src/main/java/com/okysoft/annictim/presentation/person/PersonActivity.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/person/PersonActivity.kt
@@ -2,12 +2,14 @@ package com.okysoft.annictim.presentation.person
 
 import android.content.Context
 import android.content.Intent
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.okysoft.annictim.R
 import com.okysoft.annictim.databinding.ActivityPersonBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PersonActivity : AppCompatActivity() {
 
     companion object {
@@ -27,20 +29,15 @@ class PersonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_casts)
 
         val binding = DataBindingUtil.setContentView<ActivityPersonBinding>(this, R.layout.activity_person)
-        binding.toolbar.title = getString(R.string.search)
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.let {
-            it.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (savedInstanceState == null) {
+            val personId = intent.getIntExtra(PERSON_ID, -1)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, PersonFragment.newInstance(personId))
+                .commit()
         }
-
-//        if (savedInstanceState == null) {
-//            val personId = intent.getIntExtra(PERSON_ID, -1)
-//            val castRequestParam = CastRequestParams(workId = workId)
-//            supportFragmentManager
-//                .beginTransaction()
-//                .replace(R.id.container, CastsFragment.newInstance(castRequestParam))
-//                .commit()
-//        }
     }
 
 }
--- app/src/main/java/com/okysoft/annictim/presentation/person/PersonFragment.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/person/PersonFragment.kt
@@ -2,23 +2,71 @@ package com.okysoft.annictim.presentation.person
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.okysoft.annictim.R
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.okysoft.annictim.extension.openUrl
+import com.okysoft.annictim.util.compose.Center
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class PersonFragment : Fragment() {
 
+    val personId: Int
+        get() = arguments?.getInt(PERSON_ID) ?: 0
+
+    @Inject
+    lateinit var factory: PersonViewModelImpl.Factory
+    private val viewModel: PersonViewModel by viewModels {
+        PersonViewModelImpl.provideFactory(factory, personId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.fetch()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_person, container, false)
+                              savedInstanceState: Bundle?): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val state: State<PersonViewModel.State> = viewModel.stateFlow.observeAsState(PersonViewModel.State.Loading)
+                when (val stateValue = state.value) {
+                    is PersonViewModel.State.Loading -> {
+                        Center(modifier = Modifier.fillMaxWidth(0.4f)) {
+                            CircularProgressIndicator()
+                        }
+                    }
+                    is PersonViewModel.State.Error -> {
+                        ErrorScreen(resume = { viewModel.fetch() })
+                    }
+                    is PersonViewModel.State.Success -> {
+                        if (stateValue.listItem.isEmpty()) {
+                            EmptyScreen()
+                            return@setContent
+                        }
+                        PersonList(stateValue.listItem) { url ->
+                            openUrl(url)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     companion object {
-        val TAG = PersonFragment::class.java.simpleName
+        val TAG: String = PersonFragment::class.java.simpleName
         const val PERSON_ID = "PERSON_ID"
 
         fun newInstance(person_id: Int): PersonFragment = PersonFragment().apply {
