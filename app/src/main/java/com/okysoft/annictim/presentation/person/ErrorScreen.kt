af526ed3bb8dadf7752a84f1539b283c8b0a7212
oyuk <okysoft68@gmail.com>
2021-10-01 16:57:31 +0900

    error時のリトライ用画面

---
+++ app/src/main/java/com/okysoft/annictim/presentation/person/ErrorScreen.kt
@@ -0,0 +1,36 @@
+package com.okysoft.annictim.presentation.person
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.okysoft.annictim.util.compose.Center
+
+@Preview(showSystemUi = true)
+@Composable
+fun ErrorScreen(
+    message: String = "エラーが発生しました",
+    resume: () -> Unit = {}
+) {
+    Center {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = message,
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
+            Button(onClick = resume,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text(text = "リロード")
+            }
+        }
+    }
+}
