3e1bab54585fa96abf789653c3ea49ff42ffd962
oyuk <okysoft68@gmail.com>
2021-10-04 19:54:34 +0900

    coroutineにした

--- infra/src/main/java/com/okysoft/infra/impl/WorkFeedRepositoryImpl.kt
+++ infra/src/main/java/com/okysoft/infra/impl/WorkFeedRepositoryImpl.kt
@@ -2,31 +2,35 @@ package com.okysoft.infra.impl
 
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Input
-import com.apollographql.apollo.coroutines.toFlow
+import com.apollographql.apollo.api.toInput
+import com.apollographql.apollo.coroutines.await
 import com.okysoft.common.PaginatableResponse
 import com.okysoft.infra.WorkFeedsQuery
 import com.okysoft.infra.fragment.WorkFeed
 import com.okysoft.infra.repository.WorkFeedRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class WorkFeedRepositoryImpl(private val apoloClient: ApolloClient): WorkFeedRepository {
 
-    @ExperimentalCoroutinesApi
-    override fun fetch(season: String, after: String?): Flow<PaginatableResponse<WorkFeed>> {
-        return apoloClient.query(WorkFeedsQuery(season, Input.fromNullable(after))).toFlow()
-            .map {
-                val hasNextPage = it.data?.searchWorks?.pageInfo?.fragments?.pageInfo?.hasNextPage
-                val nextCursor = it.data?.searchWorks?.pageInfo?.fragments?.pageInfo?.endCursor
-                val list = it.data?.searchWorks?.nodes?.let { l ->
-                    l.mapNotNull { node -> node?.fragments?.workFeed }
-                }
-                if (hasNextPage != null && nextCursor != null && list != null) {
-                    return@map PaginatableResponse(hasNextPage, nextCursor, list)
-                }
-                throw NullPointerException()
-            }
+    override suspend fun fetch(season: String?, title: String?, after: String?): PaginatableResponse<WorkFeed> {
+        val seasons: List<String> = season?.let {
+            listOf(it)
+        } ?: emptyList()
+        val titles: List<String> = title?.let {
+            listOf(it)
+        } ?: emptyList()
+        val response = apoloClient.query(WorkFeedsQuery(
+            seasons.toInput(),
+            titles.toInput(),
+            Input.fromNullable(after))).await()
+        val hasNextPage = response.data?.searchWorks?.pageInfo?.fragments?.pageInfo?.hasNextPage
+        val nextCursor = response.data?.searchWorks?.pageInfo?.fragments?.pageInfo?.endCursor
+        val list = response.data?.searchWorks?.nodes?.let { l ->
+            l.mapNotNull { node -> node?.fragments?.workFeed }
+        }
+        if (hasNextPage != null && nextCursor != null && list != null) {
+            return PaginatableResponse(hasNextPage, nextCursor, list)
+        }
+        throw NullPointerException()
     }
 
 }
--- infra/src/main/java/com/okysoft/infra/repository/WorkFeedRepository.kt
+++ infra/src/main/java/com/okysoft/infra/repository/WorkFeedRepository.kt
@@ -2,8 +2,7 @@ package com.okysoft.infra.repository
 
 import com.okysoft.common.PaginatableResponse
 import com.okysoft.infra.fragment.WorkFeed
-import kotlinx.coroutines.flow.Flow
 
 interface WorkFeedRepository {
-    fun fetch(season: String, after: String?): Flow<PaginatableResponse<WorkFeed>>
+    suspend fun fetch(season: String?, title: String?, after: String?): PaginatableResponse<WorkFeed>
 }
