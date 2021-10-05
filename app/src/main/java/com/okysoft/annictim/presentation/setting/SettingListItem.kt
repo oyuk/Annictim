250cca6c7fad927fbab1df29112b827c3ac168b1
oyuk <okysoft68@gmail.com>
2021-10-05 14:17:16 +0900

    setting画面のリスト用のdata class

---
+++ app/src/main/java/com/okysoft/annictim/presentation/setting/SettingListItem.kt
@@ -0,0 +1,3 @@
+package com.okysoft.annictim.presentation.setting
+
+data class SettingListItem(val title: String, val action: () -> Unit)
---
+++ app/src/main/java/com/okysoft/annictim/presentation/setting/SettingListSection.kt
@@ -0,0 +1,3 @@
+package com.okysoft.annictim.presentation.setting
+
+data class SettingListSection(val title: String, val items: List<SettingListItem>)
