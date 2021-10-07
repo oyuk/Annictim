bc5edb612d8211e88358b4422e54acad7097b878
oyuk <okysoft68@gmail.com>
2021-10-07 13:04:01 +0900

    composeを使用して表示する

--- app/src/main/java/com/okysoft/annictim/presentation/episode/EpisodesFragment.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/episode/EpisodesFragment.kt
@@ -5,21 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.okysoft.annictim.R
-import com.okysoft.annictim.databinding.FragmentEpisodesBinding
 import com.okysoft.annictim.presentation.record.RecordsActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class EpisodesFragment : Fragment() {
-
-    private lateinit var binding: FragmentEpisodesBinding
 
     val workId: Int
         get() = arguments?.getInt(WORK_ID) ?: -1
@@ -30,28 +25,21 @@ class EpisodesFragment : Fragment() {
         EpisodesViewModel.provideFactory(viewModelFactory, workId)
     }
 
-    private val adapter = EpisodesAdapter()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_episodes, container, false)
-
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                EpisodesView(viewModel)
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val layoutManager = LinearLayoutManager(activity)
-        binding.recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = adapter
-        viewModel.episodes.observe(viewLifecycleOwner, Observer {
-            adapter.submitList(it)
-        })
-        adapter.onClick.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                startActivity(RecordsActivity.createIntent(requireActivity(), it.id))
-            }
-        })
+        viewModel.tapped.observe(viewLifecycleOwner) {
+            startActivity(RecordsActivity.createIntent(requireActivity(), it.id))
+        }
         viewModel.fetch()
     }
 
