15205094ac0fa253a2bf4785ae6774978a154609
oyuk <okysoft68@gmail.com>
2021-10-10 13:35:41 +0900

    imutableをつけた方が良いぽい

--- app/src/main/java/com/okysoft/annictim/presentation/person/PersonListItem.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/person/PersonListItem.kt
@@ -1,3 +1,6 @@
 package com.okysoft.annictim.presentation.person
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class PersonListItem(val title: String, val content: String, val linkable: Boolean = false)
