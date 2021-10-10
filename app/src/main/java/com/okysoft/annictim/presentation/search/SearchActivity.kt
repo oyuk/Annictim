477d0dd860bd8b926d2eaa6e0bf6b606e66cf126
oyuk <okysoft68@gmail.com>
2021-10-10 13:36:22 +0900

    activityだけで処理を完結させる

--- app/src/main/java/com/okysoft/annictim/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/search/SearchActivity.kt
@@ -3,14 +3,22 @@ package com.okysoft.annictim.presentation.search
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.okysoft.annictim.R
-import com.okysoft.annictim.databinding.ActivitySearchBinding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.Observer
+import com.okysoft.annictim.AnnictimTheme
+import com.okysoft.annictim.presentation.works.WorksActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchActivity : AppCompatActivity() {
+
+    private val viewModel: SearchViewModel by viewModels()
 
     companion object {
         fun createIntent(activity: Context) = Intent(activity, SearchActivity::class.java)
@@ -18,21 +26,35 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
 
-        val binding = DataBindingUtil.setContentView<ActivitySearchBinding>(this, R.layout.activity_search)
-        binding.toolbar.title = getString(R.string.search)
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.let {
-            it.setDisplayHomeAsUpEnabled(true)
-        }
+        viewModel.transitionTo.observe(this , Observer {
+            startActivity(WorksActivity.createIntent(this, "検索", it!!))
+        })
 
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.container, SearchFragment.newInstance())
-                .commit()
+        setContent {
+            AnnictimTheme {
+                Scaffold(
+                    topBar = { AppBar() }
+                ) {
+                    SearchScreen {
+                        viewModel.search(it)
+                    }
+                }
+            }
         }
+    }
+
+    @Composable
+    fun AppBar() {
+        TopAppBar(
+            title = { Text(text = "検索") },
+            navigationIcon = {
+                IconButton(onClick = { finish() }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back")
+                }
+            }
+        )
     }
 
 }
