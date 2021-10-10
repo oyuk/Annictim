2543ccf301626cfe17020665a8daf19dd85c476e
oyuk <okysoft68@gmail.com>
2021-10-10 13:33:58 +0900

    DI用の関数が変わったので修正

--- app/src/main/java/com/okysoft/annictim/presentation/works/WorksViewModel.kt
+++ app/src/main/java/com/okysoft/annictim/presentation/works/WorksViewModel.kt
@@ -1,5 +1,6 @@
 package com.okysoft.annictim.presentation.works
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -7,6 +8,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.okysoft.annictim.extension.extractActivity
 import com.okysoft.data.WorkRequestParams
 import com.okysoft.infra.WorkFeedPagingSource
 import com.okysoft.infra.fragment.WorkFeed
@@ -14,6 +16,10 @@ import com.okysoft.infra.repository.WorkFeedRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import io.reactivex.processors.PublishProcessor
 import kotlinx.coroutines.flow.Flow
 
@@ -27,43 +33,30 @@ class WorksViewModel @AssistedInject constructor(
         fun create(workRequestParams: WorkRequestParams): WorksViewModel
     }
 
+    @EntryPoint
+    @InstallIn(ActivityComponent::class)
+    interface ActivityCreatorEntryPoint {
+        fun getWorksViewModelFactory(): Factory
+    }
+
     companion object {
+
         fun provideFactory(
-            factory: Factory,
+            context: Context,
             workRequestParams: WorkRequestParams
         ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val activity = context.extractActivity()
+                val factory = EntryPoints.get(activity, ActivityCreatorEntryPoint::class.java)
+                    .getWorksViewModelFactory()
                 return factory.create(workRequestParams) as T
             }
         }
     }
 
-//    val loadMore = PublishProcessor.create<Unit>()
-    private val refresh = PublishProcessor.create<Unit>()
-
-//    @ExperimentalCoroutinesApi
-//    private val paginator = PaginatablePaginator<WorkFeed>(loadMore, refresh) { cursor, callback ->
-//        viewModelScope.launch {
-//            try {
-//            repository.fetch(workRequestParams.season ?: "", cursor)
-//                .collect {
-//                    callback(Either.Right(it))
-//                }
-//            } catch (throwable: Throwable) {
-//                callback(Either.Left(throwable))
-//            }
-//        }
-//    }
-
     val works: Flow<PagingData<WorkFeed>> = Pager(PagingConfig(pageSize = 20)) {
         WorkFeedPagingSource(repository, workRequestParams)
     }
         .flow.cachedIn(viewModelScope)
-
-    fun refresh() {
-//        refresh.onNext(Unit)
-    }
-
-
 }
